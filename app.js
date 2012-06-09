0f5e8ecbed54e7f1c9029fa76acd47d77ba5786c
Alon Valadji <alon@empeeric.com>
2012-06-10 01:53:04 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+app.listen(80);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
